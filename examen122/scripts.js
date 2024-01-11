9524fe7d4ae71426eb8bf84e843d24838e045765
jaxmin203 <155836941+jaxmin203@users.noreply.github.com>
2024-01-11 12:04:06 -0400

    Add files via upload

---
+++ examen122/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('downloadBtn').addEventListener('click', function () {
+    alert('Descargando documento');
+});
+
+document.getElementById('contactBtn').addEventListener('click', function () {
+    window.location.href = 'mailto:contact@me.com';
+});
